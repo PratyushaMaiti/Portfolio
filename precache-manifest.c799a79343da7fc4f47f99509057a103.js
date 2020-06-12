811914a21e2d1276c7f5296d223ab982f1a1fe89
Pratyusha Maiti <pratyusha.maiti@gmail.com>
2020-06-12 16:14:50 +0530

    Updates

---
+++ precache-manifest.c799a79343da7fc4f47f99509057a103.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "815fe7f559d69f1c8fb9228590242416",
+    "url": "/Portfolio/index.html"
+  },
+  {
+    "revision": "ea9a41e276a3a9886fc9",
+    "url": "/Portfolio/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "6bb64744ce24d0b410f1",
+    "url": "/Portfolio/static/js/2.18e4ac39.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Portfolio/static/js/2.18e4ac39.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ea9a41e276a3a9886fc9",
+    "url": "/Portfolio/static/js/main.0a0b3cba.chunk.js"
+  },
+  {
+    "revision": "425e952245b014fc3ffb",
+    "url": "/Portfolio/static/js/runtime-main.db605a1d.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/Portfolio/static/media/logo.5d5d9eef.svg"
+  }
+]);
